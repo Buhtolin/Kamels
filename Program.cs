466b6390cc87304fdd24f7c114a86d43f859a894
moustachecabal <vasil.kotsev@moustachecabal.com>
2023-02-27 01:34:33 +0200

    Added Device Listing

--- Program.cs
+++ Program.cs
@@ -1,20 +1,101 @@
 ﻿using HidApi;
+using System.Text;
 
 class Program
 {
+
+    const ushort logitechVendorID = 1133;
     static void Main()
     {
         Console.WriteLine("Hello, World!");
-        ListDevices();
+        ListLogitechHIDDevices();
         Console.Read();
     }
 
-    static void ListDevices()
+    //static void ListDevices()
+    //{
+    //    List<DeviceInfo> devices = Hid.Enumerate().ToList();
+    //    foreach (var deviceInfo in Hid.Enumerate())
+    //    {
+    //        using var device = deviceInfo.ConnectToDevice();
+    //        Console.WriteLine(device.GetManufacturer());
+    //    }
+    //}
+
+    static void ListLogitechHIDDevices()
     {
-        foreach (var deviceInfo in Hid.Enumerate())
+        StringBuilder sb = new StringBuilder();
+
+        List<DeviceInfo> devices = Hid.Enumerate()
+                                        .Where(x=>x.VendorId.Equals(logitechVendorID))
+                                        .Distinct()
+                                        .ToList();
+
+        devices.ForEach(x => Console.WriteLine(x.Path));
+
+        sb.AppendLine("#\tManufacturer\tProduct Desc\tBus\tProduct ID");
+
+        for (int i=0; i<devices.Count; i++)
         {
-            using var device = deviceInfo.ConnectToDevice();
-            Console.WriteLine(device.GetManufacturer());
-        }
+            string deviceInfoLine = $"{i + 1}.\t" +
+                                    $"{devices[i].ManufacturerString}\t" +
+                                    $"{devices[i].ProductString}\t" +
+                                    $"{devices[i].BusType}\t" +
+                                    $"{devices[i].ProductId}";
+
+            sb.AppendLine(deviceInfoLine);
+        };
+
+        string withFixedColumns = FixConsoleColumnWidth(sb.ToString());
+
+        string headerLine = withFixedColumns.Split("\r\n")[0];
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(headerLine);
+
+        Console.ForegroundColor = ConsoleColor.Gray;
+
+        Console.WriteLine(withFixedColumns.Substring(headerLine.Length+2));
+    }
+
+    static string FixConsoleColumnWidth(string input)
+    {
+        int columnsCount = input.Split("\r\n")[0].Split('\t').Length;
+
+        string[] columnValues = input.Replace("\r\n","\t").Split("\t");
+
+        List<int> maxColumnWidths = new List<int>();
+
+        for(int i=0; i<columnsCount; i++)
+        {
+            List<int> columnWidths = new List<int>();
+
+            for(int p=i; p<columnValues.Length; p += columnsCount)
+            {
+                columnWidths.Add(columnValues[p].Length);
+            };
+
+            maxColumnWidths.Add(columnWidths.Max());
+        };
+
+        StringBuilder rebuildSB = new StringBuilder();
+
+        for (int i = 0; i < columnValues.Length-1; i+=columnsCount)
+        {
+            for(int p=0; p<columnsCount; p++)
+            {
+                rebuildSB.Append(columnValues[i+p].PadRight(maxColumnWidths[p]));
+                if (p==(columnsCount-1))
+                {
+                    rebuildSB.Append("\r\n");
+                }
+                else
+                {
+                    rebuildSB.Append('\t');
+                };
+            };
+        };
+
+        return rebuildSB.ToString();
     }
 }
