1754602f9716a18cafb74f8ec21062721f570194
moustachecabal <vasil.kotsev@moustachecabal.com>
2023-02-26 13:04:39 +0200

    Added hidapi.net to project

--- Program.cs
+++ Program.cs
@@ -1,8 +1,20 @@
-﻿class Program
+﻿using HidApi;
+
+class Program
 {
     static void Main()
     {
         Console.WriteLine("Hello, World!");
+        ListDevices();
         Console.Read();
+    }
+
+    static void ListDevices()
+    {
+        foreach (var deviceInfo in Hid.Enumerate())
+        {
+            using var device = deviceInfo.ConnectToDevice();
+            Console.WriteLine(device.GetManufacturer());
+        }
     }
 }
